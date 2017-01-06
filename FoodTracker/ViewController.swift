4f7c8daeeac07bc621061822af8a1904e61efdd1
anirudh.r <anirudh.r@flipkart.com>
2017-01-06 15:06:25 +0530

    connect ui to code

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mealNameTextField: UITextField!
+    @IBOutlet weak var mealNameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,5 +22,8 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func se(_ sender: UIButton) {
+        mealNameLabel.text = mealNameTextField.text
+    }
 }
-
