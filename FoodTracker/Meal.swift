eae082ede35a9ca5b09e2045edb8af68568798ed
anirudh.r <anirudh.r@flipkart.com>
2017-01-23 21:08:00 +0530

    Protocol to save meals

--- FoodTracker/Meal.swift
+++ FoodTracker/Meal.swift
@@ -9,10 +9,13 @@
 import Foundation
 import UIKit
 
-class Meal {
+class Meal : NSObject, NSCoding {
     var name: String
     var mealImage : UIImage?
     var rating: Int
+    
+    static let DocumentsDirectory = FileManager().urls(for: .documentDirectory, in: .userDomainMask).first!
+    static let ArchiveURL = DocumentsDirectory.appendingPathComponent("meals")
     
     init?(name: String, mealImage: UIImage?, rating:Int) {
         guard !name.isEmpty else {
@@ -26,5 +29,28 @@ class Meal {
         self.name = name
         self.mealImage = mealImage
         self.rating = rating
+    }
+    
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(name, forKey: PropertyKey.name)
+        aCoder.encode(mealImage, forKey: PropertyKey.photo)
+        aCoder.encode(rating, forKey: PropertyKey.rating)
+    }
+    
+    required convenience init?(coder aDecoder: NSCoder) {
+        guard let name = aDecoder.decodeObject(forKey: PropertyKey.name) as? String else {
+            return nil
+        }
+        let photo = aDecoder.decodeObject(forKey: PropertyKey.photo) as? UIImage
+        
+        let rating = aDecoder.decodeInteger(forKey: PropertyKey.rating)
+     
+        self.init(name: name, mealImage: photo, rating: rating)
+    }
+    
+    struct PropertyKey {
+        static let name = "name"
+        static let photo = "photo"
+        static let rating = "rating"
     }
 }
